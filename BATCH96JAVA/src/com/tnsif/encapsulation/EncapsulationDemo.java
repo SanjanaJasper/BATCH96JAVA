0cf1e57c9eb984fdda1bdc8487da9ad2f6e254e9
Lenovo <Lenovo@DESKTOP-8HSBAI9>
2024-07-01 18:43:02 +0530

    first commit

---
+++ BATCH96JAVA/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//object creation
+				Encapsulation ob= new Encapsulation();
+				
+				ob.setAge(22);
+				ob.setName("ramya");
+				ob.setSerialNum(55);
+				System.out.println(ob);
+			}
+
+
+	}
+
